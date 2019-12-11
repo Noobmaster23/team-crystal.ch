5ba08cc80ef7744789919f3c5ba30e655e42f644
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2019-12-11 07:42:51 +0100

    testing

--- team-crystal.ch/html/javascript/test.js
+++ team-crystal.ch/html/javascript/test.js
@@ -1,1 +1,26 @@
-alert("This is an alert box!");
+// alert("Virus wird installiert...");
+// var wort = "test";
+// document.write(wort);
+// if (true) {
+//     // document.write();
+// };
+// var test = {
+//     number: 1, name: "test"
+// };
+// document.writeln(test);
+// document.writeln(test.number);
+// document.writeln(test["name"]);
+// function person(name, age, color) {
+//     this.name = name;
+//     this.age = age;
+//     this.favColor = color;
+//     this.changeName = function (name) {
+//         this.name = name
+//     }
+// };
+// var p1 = new person("Dominik", 12, "black");
+// var p2 = new person("...", -1, "green");
+// function myAlert(){
+//     alert("Hoi!");
+// }
+// setInterval(myAlert, 4000);
