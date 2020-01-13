b9a9d13481b204f3aca51d970ff961db2ee224e5
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-01-13 18:03:21 +0100

    v0.02 dropdown for mobile

--- team-crystal.ch/html/javascript/main.js
+++ team-crystal.ch/html/javascript/main.js
@@ -2,10 +2,19 @@ window.addEventListener("load", loaded);
 var counter_1 = 0;
 var interval_1;
 var is_dropdown = false;
+var is_mobile;
 
 function loaded() { //waits until the page is loaded, important for functions that need the content of the page (when the page is not fully loaded, it doesn't detect everything)
     var ddn = document.getElementById("dropdown");
     ddn.addEventListener("click", dropdown);
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) { //checks if the website is used on a mobile devise
+        is_mobile = true;
+        var sb = document.getElementById("selectionbar_desktop");
+        sb.style.display = "none";
+    } else {
+        var sb = document.getElementById("selectionbar_mobile");
+        sb.style.display = "none"
+    }
 }
 
 function dropdown() {
@@ -21,13 +30,15 @@ function dropdown() {
     }
 }
 
-function dropdown_animation() { //lines turn by 90° (more or less) animation for dropping menu down (duh)
+function dropdown_animation() { //turning animation and shows the menu
     var ddn = document.getElementById("dropdown");
-    var sb = document.getElementById("selectionbar");
+    var sb = document.getElementById("selectionbar_mobile");
     var top = sb.clientHeight / 90;
     var temp_number = 120 - sb.clientHeight + top * counter_1;
-    sb.style.top = String(temp_number) + "px";
-    sb.style.display = "initial";
+    if (is_mobile) { //if mobile device, do nothing to this
+        sb.style.top = String(temp_number) + "px";
+        sb.style.display = "initial";
+    }
     ddn.style.transform = "rotate(" + counter_1 + "deg)";
     counter_1++;
     if (counter_1 > 90) {
@@ -37,12 +48,14 @@ function dropdown_animation() { //lines turn by 90° (more or less) animation fo
     }
 }
 
-function goup_animation() { //lines turn by 90° (more or less) animation for going up with the menu (duh)
+function goup_animation() { //turning animation and shows the menu
     var ddn = document.getElementById("dropdown");
-    var sb = document.getElementById("selectionbar");
+    var sb = document.getElementById("selectionbar_mobile");
     var top = sb.clientHeight / 90;
     var temp_number = 120 - top * counter_1;
-    sb.style.top = String(temp_number) + "px";
+    if (is_mobile) { //if mobile device, do nothing to this
+        sb.style.top = String(temp_number) + "px";
+    }
     ddn.style.transform = "rotate(" + (90 - counter_1) + "deg)";
     counter_1++;
     if (counter_1 >= 90) {
